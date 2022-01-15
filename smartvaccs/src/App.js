b2613ce902a92bf1abe69f0864b36af4a25d0434
Shi Ning Le <shining@ualberta.ca>
2022-01-15 14:42:26 -0700

    added routes

--- smartvaccs/src/App.js
+++ smartvaccs/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import User from './components/User/User';
+import Pharmacy from './components/Pharmacy/Pharmacy';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/user" element={<User />} />
+        <Route path="pharmacy" element={<Pharmacy />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ smartvaccs/src/components/Pharmacy/Pharmacy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pharmacy = () => {
+  return (
+    <div>
+      <h1>Pharmacy</h1>
+    </div>
+  );
+};
+
+export default Pharmacy;
