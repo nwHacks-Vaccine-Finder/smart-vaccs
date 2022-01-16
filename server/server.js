775ffce731e0cd42fdcde9d3d8408aae4f4e7582
Shi Ning Le <shining@ualberta.ca>
2022-01-16 06:13:38 -0700

    add depolyment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 const { mongoConnect } = require('./services/mongo');
 const usersRouter = require('./routes/users/users.router');
 const pharmaciesRouter = require('./routes/pharmacies/pharmacies.router');
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/pharmacies', pharmaciesRouter);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 const server = http.createServer(app);
 async function startServer() {
