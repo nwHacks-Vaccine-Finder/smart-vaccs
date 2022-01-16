fcdcfbbc54b45fafb5b1218ecd2f7dd62923fc8e
Shi Ning Le <shining@ualberta.ca>
2022-01-15 20:04:13 -0700

    fixes

--- smartvaccs/src/components/LandingPage.js
+++ smartvaccs/src/components/LandingPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Stack, Button } from '@mui/material';
+import './LandingPage.css';
+import { fontSize } from '@mui/system';
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="container">
+      <Stack
+        spacing={5}
+        direction="row"
+        justifyContent="center"
+        style={{ left: '-200px' }}
+      >
+        <img src={'./vaccine_svg.png'} width="110px" height="110px" />
+        <img src={'./Logo.png'} width="600px" />
+      </Stack>
+
+      <Stack spacing={5}>
+        <Button id="btn" style={{ backgroundColor: '#6247AA' }}>
+          I'm a user!
+        </Button>
+        <Button id="btn" style={{ backgroundColor: '#A06CD5' }}>
+          I'm a pharmacy!
+        </Button>
+      </Stack>
     </div>
   );
 };
