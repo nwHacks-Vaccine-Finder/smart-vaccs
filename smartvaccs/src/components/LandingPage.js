fccb0b0c5e00fc1a91959bc08d0270caaee07348
Shi Ning Le <shining@ualberta.ca>
2022-01-15 22:45:13 -0700

    Update LandingPage.js

--- smartvaccs/src/components/LandingPage.js
+++ smartvaccs/src/components/LandingPage.js
@@ -24,7 +24,6 @@ const LandingPage = (props) => {
       </Stack>
 
       <Stack spacing={5}>
-<<<<<<< HEAD
         <Link
           onClick={(event) =>
             props.auth.isSignedIn === true ? null : event.preventDefault
@@ -45,14 +44,6 @@ const LandingPage = (props) => {
             I'm a pharmacy!
           </Button>
         </Link>
-=======
-        <Button id="btn" style={{ backgroundColor: '#6247AA' }}>
-          I'm a user
-        </Button>
-        <Button id="btn" style={{ backgroundColor: '#A06CD5' }}>
-          I'm a pharmacy
-        </Button>
->>>>>>> d3b3d3f5c3dd09afe3660a460615137c0216cf7a
       </Stack>
     </div>
   );
