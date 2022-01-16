e48c68e37967b3af3b1e493d97aa59e2cc902b49
Shi Ning Le <shining@ualberta.ca>
2022-01-15 19:57:22 -0700

    fixes

---
+++ smartvaccs/src/components/Pharmacy/Pharmacy.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './PharmacyStyles.css';
+
+const Pharmacy = () => {
+  return (
+    <div class="row">
+      <div class="column">
+        <div class="Pfizer">
+          <h2 class="pfizertitle">Pfizer</h2>
+          <p>Data..</p>
+        </div>
+        <div class="Moderna">
+          <h2>Moderna</h2>
+          <p>Data..</p>
+        </div>
+        <div class="AstraZeneca">
+          <h2>Astra Zeneca</h2>
+          <p>Data..</p>
+        </div>
+        <div class="PfizerPED">
+          <h2>Pfizer (PED)</h2>
+          <p>Data..</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pharmacy;
